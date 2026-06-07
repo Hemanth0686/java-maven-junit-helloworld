33532a85cfdca50eb3130ceea78971cde38659b9
Hemanth Kumar Nalla <161476106+Hemanth0686@users.noreply.github.com>
2026-06-07 10:44:16 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
  */
 public class Hello {
 
-    static final String HELLO = "Hello iam Hemanth!";
+    static final String HELLO = "Hello iam Hemanth! Good morning";
 
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
 
